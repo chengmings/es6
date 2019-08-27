08b72c4809a52feccea7211665bb97890c47e4dd
chengmings <741957388@qq.com>
2019-08-27 19:13:05 +0800

    1

--- Generator 函数.js
+++ Generator 函数.js
@@ -69,20 +69,41 @@
 //   console.log(f);
 // }
 
-var arr = [1, [[2, 3], 4], [5, 6]];
+// var arr = [1, [[2, 3], 4], [5, 6]];
+// var flat = function* (a) {
+//   var length = a.length;
+//   for (var i = 0; i < length; i++) {
+//     var item = a[i];
+//     if (typeof item !== 'number') {
+//       yield* flat(item);
+//     } else {
+//       yield item;
+//     }
+//   }
+// };
 
-var flat = function* (a) {
-  var length = a.length;
-  for (var i = 0; i < length; i++) {
-    var item = a[i];
-    if (typeof item !== 'number') {
-      yield* flat(item);
-    } else {
-      yield item;
-    }
-  }
-};
+// for (var f of flat(arr)) {
+//   console.log(f);
+// }
 
-for (var f of flat(arr)) {
-  console.log(f);
-}
+// 与 Iterator 接口的关系(什么鬼)
+// Generator 函数就是遍历器生成函数，因此可以把 Generator 赋值给对象的Symbol.iterator属性，从而使得该对象具有 Iterator 接口。
+
+// var myIterable = {};
+// myIterable[Symbol.iterator] = function* () {
+//   yield 1;
+//   yield 2;
+//   yield 3;
+// };
+// console.log(myIterable)
+// console.log([...myIterable]) // [1, 2, 3]
+//Generator 函数赋值给Symbol.iterator属性，从而使得myIterable对象具有了 Iterator 接口，可以被...运算符遍历了。
+
+// function* gen(){
+//   // some code
+// }
+// var g = gen();
+// console.log(g[Symbol.iterator]() === g) // true
+// gen是一个 Generator 函数，调用它会生成一个遍历器对象g。它的Symbol.iterator属性，也是一个遍历器对象生成函数，执行后返回它自己。
+
+// next 方法的参数
